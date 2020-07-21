75c78b2d15dfb4df3ff19a2728e3aaee23a38aeb
spiderman@xhift.co.kr <spiderman@xhift.co.kr>
2020-07-21 23:43:15 +0900

    removed unnecessary imports

--- src/test/java/com/example/demo/CustomerControllerThruServerTests.java
+++ src/test/java/com/example/demo/CustomerControllerThruServerTests.java
@@ -1,22 +1,14 @@
 package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.web.servlet.MvcResult;
-
 import com.example.demo.dao.CustomerRepository;
 import com.example.demo.entity.Customer;
-
-import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CustomerControllerThruServerTests {
