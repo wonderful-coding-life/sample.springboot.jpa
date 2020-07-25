3b91c207caace842f7c9f7680b1e3a0709493353
spiderman@xhift.co.kr <spiderman@xhift.co.kr>
2020-07-25 20:25:55 +0900

    added AppKey

--- src/main/java/com/example/demo/config/SpringFoxConfig.java
+++ src/main/java/com/example/demo/config/SpringFoxConfig.java
@@ -1,12 +1,19 @@
 package com.example.demo.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -21,6 +28,8 @@ public class SpringFoxConfig {
 				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
 				.build()
 				.apiInfo(apiInfo())
+				.securityContexts(Lists.newArrayList(securityContext()))
+				.securitySchemes(Lists.newArrayList(apiKey()))
 				.useDefaultResponseMessages(false);
 	}
 	
@@ -30,5 +39,18 @@ public class SpringFoxConfig {
 				.version("V1.0")
 				.description("고객 정보 관리는 모두 이 문서에 기술된 API만을 사용하세요.\n문의: support@mycompany.com")
 				.build();
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()) .build();
+	}
+	
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope[] authorizationScopes = { new AuthorizationScope("global", "accessEverything") };
+		return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+	}
+	
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "Authorization", "header");
 	}
 }
